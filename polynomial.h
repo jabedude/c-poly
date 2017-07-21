4fed74bff8b6bb653984b1c1663399bc9f4e4c1e
Josh Abraham <sinisterpatrician@gmail.com>
2017-07-21 09:53:11 -0400

    Change order of functions in header to match requirements

--- polynomial.h
+++ polynomial.h
@@ -20,9 +20,9 @@ void poly_print(const polynomial *list);
 char *poly_to_string(const polynomial *p);
 polynomial *poly_add(const polynomial *a, const polynomial *b);
 polynomial *poly_sub(const polynomial *a, const polynomial *b);
-void poly_iterate(polynomial *p, void (*transform)(struct term *));
 bool poly_equal(const polynomial *a, const polynomial *b);
 double poly_eval(const polynomial *a, double x);
+void poly_iterate(polynomial *p, void (*transform)(struct term *));
 
 
 void append(polynomial *list, int data);
