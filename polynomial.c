abf466c84bdf656d60cae670d41822a3dc8ff72b
Josh Abraham <sinisterpatrician@gmail.com>
2017-07-20 22:18:11 -0400

    Add poly_add() function

--- polynomial.c
+++ polynomial.c
@@ -1,12 +1,16 @@
 #include <stdlib.h>
+#include <stdbool.h>
 #include <stdio.h>
 
 #include "polynomial.h"
 
+/* Static prototypes */
+static int term_count(const polynomial *eqn);
+
 polynomial *term_create(int coeff, unsigned int exp)
 {
     polynomial *head = malloc(sizeof(polynomial));
-    
+
     if (head) {
         head->coeff = coeff;
         head->exp   = exp;
@@ -31,21 +35,106 @@ void poly_print(const polynomial *list)
         printf("List empty\n");
         return;
     }
-    
+
     if (list->coeff) {
         printf("%c%d", list->coeff > 0 ? '+' : '\0', list->coeff);
         if (list->exp > 1)
             printf("x^%d", list->exp);
         else if (list->exp == 1)
             printf("x");
-        
+        else if (list->exp == 0) {
+            putchar('\n');
+            return;
+        }
+
         printf(" ");
     }
-    
+
     poly_print(list->next);
 }
 
-//char *poly_to_string
+char *poly_to_string(const polynomial *p) /* TODO: exact number of bytes needed */
+{
+    if (!p) {
+        printf("List empty\n");
+        return NULL;
+    }
+
+    polynomial *tmp = (polynomial *) p;
+    int len = term_count(tmp);
+    //printf("DEBUG: count is %d\n", len);
+    //char *s = malloc(sizeof(char) * (len * 5) + 1);
+    char *s = (char *) "";
+
+    for (int i = 0; i < len; i++) {
+        asprintf(&s, "%s%c%d", s, tmp->coeff > 0 ? '+' : '\0', tmp->coeff);
+        if (tmp->exp > 1)
+            asprintf(&s, "%sx^%d ", s, tmp->exp);
+        else if (tmp->exp == 1)
+            asprintf(&s, "%sx ", s);
+        tmp = tmp->next;
+    }
+/*
+    while (tmp) {
+        asprintf(&s, "%c%d ", tmp->coeff > 0 ? '+' : '\0', tmp->coeff);
+        if (tmp->exp > 1)
+            asprintf(&s, "x^%d ", tmp->exp);
+        else if (tmp->exp == 1)
+            asprintf(&s, "x ");
+        tmp = tmp->next;
+    }
+*/
+    return s;
+}
+
+polynomial *poly_add(const polynomial *a, const polynomial *b)
+{
+    int a_len = term_count(a);
+    int b_len = term_count(b);
+    polynomial *a_head = (polynomial *) a;
+    polynomial *b_head = (polynomial *) b;
+    polynomial *ret = term_create(0,0U);
+    polynomial *head = ret;
+
+    while (a_head->next && b_head->next) {
+
+        if (a_head->exp > b_head->exp) { /* If a is greater than b */
+            ret->coeff = a_head->coeff;
+            ret->exp = a_head->exp;
+            a_head = a_head->next;
+        } else if (b_head->exp > a_head->exp) { /* If b is greater than a */
+            ret->coeff = b_head->coeff;
+            ret->exp = b_head->exp;
+            b_head = b_head->next;
+        } else { /* Exponents are the same */
+            ret->exp = a_head->exp;
+            ret->coeff = a_head->coeff + b_head->coeff;
+            a_head = a_head->next;
+            b_head = b_head->next;
+        }
+
+        ret->next = term_create(0, 0U);
+        ret = ret->next;
+    }
+
+    while (a_head->next || b_head->next) {
+        if (a_head->next) {
+            ret->coeff = a_head->coeff;
+            ret->exp = a_head->exp;
+            a_head = a_head->next;
+        }
+        if (b_head->exp > a_head->exp) {
+            ret->coeff = b_head->coeff;
+            ret->exp = b_head->exp;
+            b_head = b_head->next;
+        }
+
+        ret->next = term_create(0, 0U);
+        ret = ret->next;
+    }
+
+    return head;
+}
 
 void append(polynomial *list, int data)
 {
@@ -92,4 +181,17 @@ void delete_from_list(polynomial **list, int position)
         }
     }
 
+}
+
+static int term_count(const polynomial *eqn)
+{
+    int c = 0;
+    polynomial *tmp = (polynomial *) eqn;
+
+    while (tmp) {
+        c++;
+        tmp = tmp->next;
+    }
+
+    return c;
 }
--- polynomial.h
+++ polynomial.h
@@ -13,6 +13,7 @@ polynomial *term_create(int coeff, unsigned int esp);
 void poly_destroy(polynomial *list);
 void poly_print(const polynomial *list);
 char *poly_to_string(const polynomial *p);
+polynomial *poly_add(const polynomial *a, const polynomial *b);
 
 void append(polynomial *list, int data);
 void delete_from_list(polynomial **list, int position);
